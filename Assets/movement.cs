bf1065db199a88a6df90686ced61d12e19064851
lukasnydegger <nydegger.lukas@gmail.com>
2023-07-04 10:31:58 +0200

    init

---
+++ Assets/movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+	// Update is called once per frame
+	public float speed = 1;
+
+	void Update()
+	{
+		var position = new Vector3();
+		if(Input.GetKey(KeyCode.W))
+		{
+			position.y += speed;
+		}
+
+		if(Input.GetKey(KeyCode.A)) {
+			position.x -= speed;
+		}
+
+		if(Input.GetKey(KeyCode.S)) {
+			position.y -= speed;
+		}
+
+		if(Input.GetKey(KeyCode.D)) {
+			position.x += speed;
+		}
+
+		transform.position += position;
+	}
+}
